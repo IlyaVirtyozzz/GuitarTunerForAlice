7e98b76c3263ee4ad0c1d9a7f9d637b41f8ffccb
itookyourboo <Wignorbo@yandex.ru>
2020-01-12 14:58:21 +0300

    Залил звуки и иконки

--- constants.py
+++ constants.py
@@ -8,52 +8,52 @@ STATE_ELECTRO = 4
 GUITARS = [
     {
         'name': 'Акустическая',
-        'icon': '213044/bea4dba88928102ebccb',
+        'icon': '1533899/8cbbe21a32aca1972c9d',
         'state': STATE_ACOUSTIC,
         'strings': {
-            '1': 'sound1',
-            '2': 'sound2',
-            '3': 'sound3',
-            '4': 'sound4',
-            '5': 'sound5',
-            '6': 'sound6',
+            '1': 'bb98b72a-903e-466f-82fe-7d20d0360ea2',
+            '2': 'f15f0660-cd93-450c-a354-4c990bc2f18f',
+            '3': '1768d430-fbe3-4b72-ba2d-df0129affb5a',
+            '4': '05e65ca5-3e33-436f-ad1b-dcd4c8d28843',
+            '5': '951b7be5-1ad5-41e8-8441-e9032f40c292',
+            '6': 'b785a91e-df91-4191-8cbb-bd0534f523da',
         }
     },
     {
         'name': 'Классическая',
-        'icon': '213044/bea4dba88928102ebccb',
+        'icon': '965417/2b3ff6d7831b5902fcd7',
         'state': STATE_CLASSIC,
         'strings': {
-            '1': 'sound1',
-            '2': 'sound2',
-            '3': 'sound3',
-            '4': 'sound4',
-            '5': 'sound5',
-            '6': 'sound6',
+            '1': '1821f77f-b05f-42ef-9c0d-840833a5e67e',
+            '2': 'aa77384e-9bfb-4ae3-9770-f4ce9ac95dca',
+            '3': 'e2f2fbe5-37b5-48f4-b90e-98b21ebea4fc',
+            '4': 'ab80d8b8-940a-4491-baab-96b84a2e23b8',
+            '5': 'ab931229-94ac-4969-8df9-ebeb6186543a',
+            '6': '02dbaf91-d327-4ba1-bfaf-fd46d904f745',
         }
     },
     {
         'name': 'Бас-гитара',
-        'icon': '213044/bea4dba88928102ebccb',
+        'icon': '1521359/ede44a822886ffaab519',
         'state': STATE_BAS,
         'strings': {
-            '1': 'sound1',
-            '2': 'sound2',
-            '3': 'sound3',
-            '4': 'sound4'
+            '1': 'a97950b6-a1a3-48c5-9db5-c4e20b269dc6',
+            '2': 'de6b4aaf-bb30-494f-8864-386ef8b56ada',
+            '3': '8583eba9-a03e-4867-8441-12f2126c446a',
+            '4': '3818b0dd-8078-4d19-81c2-d1a175f1a118'
         }
     },
     {
         'name': 'Электрогитара',
-        'icon': '213044/bea4dba88928102ebccb',
+        'icon': '1540737/e8465bb25eb2f8359792',
         'state': STATE_ELECTRO,
         'strings': {
-            '1': 'sound1',
-            '2': 'sound2',
-            '3': 'sound3',
-            '4': 'sound4',
-            '5': 'sound5',
-            '6': 'sound6',
+            '1': '6d861940-861b-4644-b9c0-ed7256f0ebb4',
+            '2': 'f5fe6872-4883-4a29-b649-d96fb3a3cb4b',
+            '3': 'c7344ba6-ae48-4b03-a557-37861792fe51',
+            '4': '7c8026b2-1a88-4fb5-89ba-cb69752c141f',
+            '5': 'df07de0f-9752-4019-a457-433fea1d950e',
+            '6': 'a01aa6b4-e99a-40bb-8866-8489ed3d944a',
         }
     }
 ]
